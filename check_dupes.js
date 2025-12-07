b19efb424dafdaa920870a88c155fcab4b2cbb5c
adarshanand67 <adarshan20302@gmail.com>
2025-12-07 17:23:55 +0530

    chore: remove all test dependencies and artifacts

--- check_dupes.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const data = require("./data/entertainment.json");
-
-const titles = data.map((item) => item.title);
-const duplicates = titles.filter((item, index) => titles.indexOf(item) !== index);
-
-console.log("Duplicate titles:", duplicates);
