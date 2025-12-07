0f20b862520fce7bc3e1a3acdf50061f6b6b8071
adarshanand67 <adarshan20302@gmail.com>
2025-12-07 05:40:59 +0530

    feat: make glitch effect permanent

--- components/ui/GlitchText.tsx
+++ components/ui/GlitchText.tsx
@@ -15,13 +15,13 @@ export const GlitchText = ({
         >
             <span className="relative z-10 block">{text}</span>
             <span
-                className="absolute top-0 left-0 -z-10 w-full h-full text-red-500 opacity-0 group-hover:opacity-100 group-hover:animate-glitch-1 select-none"
+                className="absolute top-0 left-0 -z-10 w-full h-full text-red-500 opacity-70 animate-glitch-1 select-none"
                 aria-hidden="true"
             >
                 {text}
             </span>
             <span
-                className="absolute top-0 left-0 -z-10 w-full h-full text-blue-500 opacity-0 group-hover:opacity-100 group-hover:animate-glitch-2 select-none"
+                className="absolute top-0 left-0 -z-10 w-full h-full text-blue-500 opacity-70 animate-glitch-2 select-none"
                 aria-hidden="true"
             >
                 {text}
