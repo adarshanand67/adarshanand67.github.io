f678c8e09f01f1e826a5be85e399cf4f0570cb84
adarshanand67 <adarshan20302@gmail.com>
2025-12-10 14:12:39 +0530

    fix: enable anime poster images from IMDb
    - Added m.media-amazon.com to Next.js remotePatterns
    - Allows loading anime poster images in music player
    - Images will now display correctly
    
    Note: Dev server needs restart for config changes to take effect

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'img.logo.dev',
             },
+            {
+                protocol: 'https',
+                hostname: 'm.media-amazon.com',
+            },
         ],
     },
     basePath: '',
